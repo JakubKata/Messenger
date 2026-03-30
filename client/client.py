94b139c080a0c6cd265a48f520c67c8f3ecafd94
JakubKata <jakubkata0@gmail.com>
2026-03-30 18:32:45 +0200

    add database

--- client/client.py
+++ client/client.py
@@ -1,50 +1,74 @@
 import socket
 import threading
 import os
-import time
 from dotenv import load_dotenv
 
+#.env
 load_dotenv()
 ip = os.getenv("SERVER_IP")
 port = int(os.getenv("SERVER_PORT"))
 key = os.getenv("SECRET_KEY")
 
+#functions
 def receive_messages(client_socket):
     while True:
-        message = client_socket.recv(1024).decode()
+        message = client_socket.recv(4096).decode()
         if not message:
             break
-        if message == "ack":
-            print("Message sent successfully.")
-        elif message == "client is not active":
-            print("The destination client is not active.")
+        parts = message.split("|")
+        command = parts[0]
+        
+        if command == "ACK":
+            if parts[1] == "ACK":
+                print("Message sent successfully.")
+            elif parts[1] == "NACK":
+                print("Failed to send the message. The destination client is not active.")
+        elif command == "MSG":
+            print(f"Message from id:{parts[1]} name:{parts[2]}: {parts[3]}")
         else:    
             print(message)
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
-    client_socket.connect((ip, port))
+def send_message(client_socket):
+    while True:
+        destination_id = input("Enter the destination client ID (or 'ACTIVE' to see active clients): ")
+        if destination_id == "ACTIVE":
+            client_socket.send("ACTIVE".encode())
+            continue
+        message = input("Enter your message: ")
+        client_socket.send(f"MSG|{destination_id}|{message}".encode())
+        
 
+def authenticate(client_socket):
     client_socket.send(key.encode())
-    if "ack" ==client_socket.recv(1024).decode():
+    if "NACK" == client_socket.recv(1024).decode():
+        return False
+    elif "ACK" == client_socket.recv(1024).decode():
         client_id = input("Enter your client ID: ")
         client_socket.send(client_id.encode())
         response = client_socket.recv(1024).decode()
-        if response == "new client":
-            name = input("Enter your name: ")
-            client_socket.send(name.encode())
+
+        if response == "NEW":
+            name = input("Enter your NEW name: ")
+            password = input("Enter your NEW password: ")
+            client_socket.send(f"{name}|{password}".encode())
         else:
-            print(f"Welcome back, {response}!")
+            password = input("Enter your password: ")
+            client_socket.send(password.encode())
         
-        threading.Thread(target=receive_messages, args=(client_socket,), daemon=True).start()
-
-        while True:
-            destination_id = input("Enter the destination client ID (or '??' to see active clients): ")
-            client_socket.send(destination_id.encode())
-            if destination_id == "??":
-                time.sleep(1)
-                continue
-            message = input("Enter your message: ")
-            client_socket.send(message.encode())
-        
+        response = client_socket.recv(1024).decode()
+        if response.split("|")[0] == "ACK":
+            name = response.split("|")[1]
+            return True
+        else:
+            print("Authentication failed.")
+            return False
             
+while True:
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+        client_socket.connect((ip, port))
 
+        if authenticate(client_socket):            
+            threading.Thread(target=receive_messages, args=(client_socket,), daemon=True).start()
+            send_message(client_socket)
+        else:
+            client_socket.close()
--- server/server.py
+++ server/server.py
@@ -1,79 +1,127 @@
 import socket
+import sqlite3
 import threading
 import json
 import os
 from dotenv import load_dotenv
 
+#.env
 load_dotenv()
-
 ip = os.getenv("SERVER_IP")
 port = int(os.getenv("SERVER_PORT"))
 key = os.getenv("SECRET_KEY")
-active_clients = {}
 
-with open("user_database.json", "r",encoding="utf-8") as f:
-    user_database = json.load(f)
+#.jsom
+with open("config.json", "r",encoding="utf-8") as f:
+    config = json.load(f)
+
+#database
+
+conn = sqlite3.connect("clients.db")
+cursor = conn.cursor()
+
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS users (
+        client_id TEXT PRIMARY KEY,
+        name TEXT NOT NULL,
+        password TEXT NOT NULL
+    )""")
+conn.commit()
+conn.close()
+
+#functions
+active_clients = {} #active_client = {client_id : [client_socket, client_address, name, password]}
 
 def is_existing_client(client_id):
-    if client_id in user_database:
-        return True
-    else:
+    if get_client(client_id) == None:
         return False
+    else:
+        return True
+    
+def get_client(client_id):
+    conn = sqlite3.connect("clients.db")
+    cursor = conn.cursor()
+    cursor.execute("SELECT name, password FROM users WHERE client_id = ?", (client_id,))
+    result = cursor.fetchone()
+    conn.close()
+    return result
 
-def new_client_id(client_id, name):
+def new_client(client_id, name, password):
+    conn = sqlite3.connect("clients.db")
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO users (client_id, name, password) VALUES (?, ?, ?)", (client_id, name, password))
+    conn.commit()
+    conn.close()
 
-    new_user = {"user_name": name}
-    user_database[client_id] = new_user
-    with open("user_database.json", "w", encoding="utf-8") as f:
-        json.dump(user_database, f)
+def handle_client(client_socket, client_address):
+    #authentication    
+    client_key = client_socket.recv(1024).decode()
+    if client_key != key:
+        client_socket.send("NACK".encode())
+        client_socket.close()
+        return
+    else:
+        client_socket.send("ACK".encode())
 
-def handle_client(client_socket, client_address):        
-        client_key = client_socket.recv(1024).decode()
-        if client_key == key:
-            client_socket.send("ack".encode())
-            client_id = client_socket.recv(1024).decode()
-            if is_existing_client(client_id):
-                client_socket.send(user_database[client_id]["user_name"].encode())
-                client_name = user_database[client_id]["user_name"]
-            else:
-                client_socket.send("new client".encode())
-                client_name = client_socket.recv(1024).decode()
-                new_client_id(client_id, client_name)  
-            active_clients[client_id] = [client_socket, client_address, client_name]            
-            source_client = active_clients[client_id]
-            
-            while True:                
-                destination_id = source_client[0].recv(1024).decode()
-                if destination_id == "??":
-                    active_clients_text = ""
-                    for client in active_clients:
-                        active_clients_text += f"{client, active_clients[client][2]}\n"
-                    source_client[0].send(active_clients_text.encode())    
-                    continue
-                if not destination_id:
-                    break
-                message = source_client[0].recv(1024).decode()
-                if not message:
-                    break
+    client_id = client_socket.recv(1024).decode()
+    if is_existing_client(client_id):
+        client = get_client(client_id)
+        active_clients[client_id] = [client_socket, client_address, *client] #active_client = {client_id : [client_socket, client_address, name, password]}
+        name = active_clients[client_id][2]
+        password = active_clients[client_id][3]
+        client_socket.send("ACK".encode())
+        client_password = client_socket.recv(1024).decode()
+    else:
+        client_socket.send("NEW".encode())
+        response = client_socket.recv(1024).decode()
+        name, password = response.split("|")
+        active_clients[client_id] = [client_socket, client_address, name, password] #active_client = {client_id : [client_socket, client_address, name, password]}
+        new_client(client_id, name, password)
+        client_password = active_clients[client_id][3]
+    
+    if password == client_password:
+        client_socket.send(f"ACK|{name}".encode()) 
 
+        #handling                   
+        while True:
+            response = client_socket.recv(4096).decode()
+            if not response:
+                break
+            parts = response.split("|")
+            command = parts[0]
+            if command == "ACTIVE":
+                active_clients_text = ""
+                for id in active_clients:
+                    active_clients_text += f"{id}, {active_clients[id][2]}\n"
+                client_socket.send(active_clients_text.encode())    
+                continue
+            if command == "MSG":
+                destination_id = parts[1]
+                message = parts[2]
                 if destination_id in active_clients:
-                    active_clients[destination_id][0].send(f"Message from {client_id}, {source_client[2]}: {message}".encode())
-                    source_client[0].send("ack".encode())
+                    active_clients[destination_id][0].send(f"MSG|{client_id}|{active_clients[client_id][2]}|{message}".encode())
+                    client_socket.send("ACK|ACK".encode())
                 else:
-                    source_client[0].send("client is not active".encode())
-                                        
-            active_clients.pop(client_id)
-            client_socket.close()
-        else:
-            client_socket.send("wrong key".encode())
-            client_socket.close()
+                    client_socket.send("ACK|NACK".encode())
+                           
+        active_clients.pop(client_id)
+        client_socket.close()
+    else:
+        client_socket.send("NACK".encode())
+        client_socket.close()
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server_socket.bind((ip, port))
-    server_socket.listen()    
+    server_socket.listen()
+
     print(f"server is listening to {ip}:{port}...")
         
     while True:
         client_socket, client_address = server_socket.accept()
+
+        if len(active_clients) >= int(config["MAX_CLIENTS"]):
+            client_socket.send("server is full".encode())
+            client_socket.close()
+            continue
         threading.Thread(target=handle_client, args=(client_socket, client_address)).start()
